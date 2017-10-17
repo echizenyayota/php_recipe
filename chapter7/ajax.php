a17b2aa042eb8f205c9a484271f76470ab2147bd
echizenyayota <virtualmisso30@gmail.com>
2017-10-17 22:38:23 +0900

    P619 JSON形式のデータを返すWeb APIを作りたい(その3)

--- chapter7/ajax.php
+++ chapter7/ajax.php
@@ -5,11 +5,16 @@
     <title>JSON形式のデータを読み込む</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     <script>
+      // 読み込みが始まった時の処理
+      // http://semooh.jp/jquery/api/ajax/jQuery.getJSON/+url,+data,+callback+/
       $(function() {
         $('#load').on("click", function(){
+          // Web API(json.php)で生成したjsonデータを取得して処理する
           $.getJSON("json.php", function(data){
             for (var i in data) {
+              // 行オブジェクトを取得
               var tr = $("<tr>");
+              // 列オブジェクトを生成して行に追加
               var td_item = $("<td>").text(data[i].item);
               tr.append(td_item);
               var td_price = $("<td>").text(data[i].price);
@@ -17,6 +22,7 @@
               var td_orders = $("<td>").text(data[i].orders);
               tr.append(td_orders);
 
+              // 行のオブジェクトをテーブルに追加
               $('#listbox').append(tr);
 
               $('#load').hide();
