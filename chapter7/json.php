7676986ef8e4a7dd9297ef828dc56e8111cd31b2
echizenyayota <virtualmisso30@gmail.com>
2017-10-20 16:54:25 +0900

    json.phpの調整（その2）

--- chapter7/json.php
+++ chapter7/json.php
@@ -1,10 +1,10 @@
 <?php
   // jQueryなどの主要ライブラリからのアクセスである
   // ブラウザからの直接アクセスでないことを確認(JSONハイジャック対策にも有効)
-  if (!isset($_SERVER['HTTP_X_REQUESTED_WITH']) ||
-    $_SERVER['HTTP_X_REQUESTED_WITH'] !== 'XMLHttpRequest' ) {
-    die (json_encode(array('status' => "不正な呼び出し"))); // dieはexitと同じ
-  }
+  // if (!isset($_SERVER['HTTP_X_REQUESTED_WITH']) ||
+  //   $_SERVER['HTTP_X_REQUESTED_WITH'] !== 'XMLHttpRequest' ) {
+  //   die (json_encode(array('status' => "不正な呼び出し"))); // dieはexitと同じ
+  // }
 
   // データを準備
   $value = array(
@@ -18,8 +18,11 @@
   header("X-Content-Type-Options: nosniff"); // XSS対策　テキストとしてと表示されるようになり、JavaScriptが実行されなくなる
 
    // 可能な限りのエスケープを行う $valueの値を読み取りにくくする。
-   echo json_encode(
-     $value,
-     JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP
-   );
+  //  echo json_encode(
+  //    $value,
+  //    JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP
+  //  );
+  echo json_encode(
+    $value
+    );
 ?>
